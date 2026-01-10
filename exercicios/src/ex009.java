0fb558ce619272612ac7ec94485776c4e85ebd8c
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-01-09 22:54:04 -0300

    Terminei o ex009

--- exercicios/src/ex009.java
+++ exercicios/src/ex009.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 
 public class ex009 {
 	
 	public static void main(String[] args) {
 		
+		int A, B;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if (A > B) {
+			if (A % B == 0) {
+				System.out.println("They're multiples!");
+			}
+			else {
+				System.out.println("They aren't multiples!");
+			}
+		}
+		else {
+			if (B % A == 0) {
+				System.out.println("They're multiples!");
+			}
+			else {
+				System.out.println("They aren't multiples!");
+			}
+		}
+		
+		sc.close();
 	}
 }
